bfb14df3434c4b460db61d92923ee3f23411ee16
FlBisquit <FlBisquit@users.noreply.github.com>
2026-04-07 15:09:07 +0500

    добавлена история сообщений в темплейт

--- Flex_messenger/chatting/views.py
+++ Flex_messenger/chatting/views.py
@@ -7,15 +7,16 @@ def room(request, chat_id):
     user = is_authorized(request)
     if not user:
         return redirect('/')
-    
+
     chat = get_object_or_404(Chat, id=chat_id, done=False)
-    
+
     if chat.is_private and user not in chat.participants.all():
         messages.error(request, 'У вас нет доступа к этому чату')
         return redirect('/users/')
-    
+
     return render(request, 'chatting/room.html', {
         'chat': chat,
         'user': user,
-        'room_name': chat_id,  
+        'room_name': chat_id,
+        'messages': chat.messages.select_related('author').all(),
     })
